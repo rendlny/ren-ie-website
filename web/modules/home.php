5c74e2298ef4f7c0405d96856b30cae91afaf9ee
rendlny <ren.delaney@gmail.com>
2019-10-28 00:30:06 +0000

    More Fixes for live server

--- web/modules/home.php
+++ web/modules/home.php
@@ -3,7 +3,7 @@
     <h2 class="text-center text-light mb-6 wow fadeInDown animation-delay-5"><strong><?=$web_data["home"]["home_title"]?></strong></h2>
     <div class="row">
       <div class="col-lg-4 order-lg-2 mb-4  center-block">
-        <img id="home-body-img" src="../assets/images/spoopyfloofer-icon2019.png" alt="" class="img-fluid center-block wow zoomIn animation-delay-12 ">
+        <img id="home-body-img" src="/web/assets/images/spoopyfloofer-icon2019.png" alt="" class="img-fluid center-block wow zoomIn animation-delay-12 ">
       </div>
       <div class="col-lg-6 order-lg-1 pr-6">
         <p class="wow fadeInLeft animation-delay-6"><?=$web_data["home"]["home_txt_1"]?></p>
