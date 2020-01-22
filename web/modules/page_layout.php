1d00a465edf669d3490ee54000141c8b00c3e7ca
Ren Dlny <ren.delaney@gmail.com>
2020-01-22 22:57:41 +0000

    moved error reporting so it is on all web pages

--- web/modules/page_layout.php
+++ web/modules/page_layout.php
@@ -1,8 +1,5 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/web/start.php';
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(E_ALL);
 
 $current_page = 'home.php';
 
