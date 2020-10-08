5da032a9dc5faa357f51baa4714d8dc410260a6a
Ren Dlny <ren.delaney@gmail.com>
2020-10-08 16:18:24 +0100

    Updated coding page

--- web/modules/coding.php
+++ web/modules/coding.php
@@ -13,8 +13,12 @@
         </div>
         <div class="card-body">
           <p class="text-center">
-            I have 2 years of experience working with PHP to create web applications
-            such as shop, inventory management, bookings & article systems.
+            I have 2 years of experience using PHP with JS, Ajax, jQuery and
+            phpMyAdmin to create web applications such as shop, inventory
+            management, bookings & article systems. Worked on major projects with
+            my team by following Scrum / Agile methodologies and the use of
+            Jira Kanban boards to deliver projects in a timely and efficient
+            manner.
           </p>
         </div>
       </div>
@@ -33,6 +37,20 @@
             which I build an object for and then print into a json file to be used
             by the web app. You can see the webscraper code on the
             <a href="https://github.com/rendlny/YokaidexWebscraper" target="_blank">github repo</a>.
+          </p>
+          <hr>
+          <p class="text-center">
+            Another Python project I have built is
+            <a href="https://github.com/rendlny/rapuli-bot" target="_blank">Rapuli-Bot</a>,
+            a Discord chat-Bot for tracking weekly turnip stock prices for the
+            game Animal Crossing New Horizons on Nintendo Switch. This bot will
+            take inputs from the user of their turnip prices and will then
+            return a link to <a href="https://turnipprophet.io/" target="_blank">TurnipProphet</a>
+            with their price data already set. Everytime they set a price,
+            the bot will update the link with their new pricing.
+            <br><br>
+            <img style="max-width:100%; border: 2px solid white;" src="https://ren.ie/web/assets/images/github/rapuli-bot/rapuli-bot-example.png" />
+            <br><small>Image of Rapuli-Bot in action, returning custom links to two users</small>
           </p>
         </div>
       </div>
