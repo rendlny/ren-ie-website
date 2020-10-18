c3ccfad429698559024ef903c263a84564d6424f
Ren Dlny <ren.delaney@gmail.com>
2020-10-18 23:17:46 +0100

    quick style change to project view page

--- web/modules/project_view.php
+++ web/modules/project_view.php
@@ -1,6 +1,6 @@
 <div class="container pt-6">
-  <div class="row">
-    <div class="col-md-12">
+  <div class="row pb-6">
+    <div class="col-md-12 bg-white pb-4">
       <?=$project->content?>
     </div>
   </div>
