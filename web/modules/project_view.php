8305c7114e51948ca4aa763bf9df1c1f23753597
Ren Dlny <ren.delaney@gmail.com>
2020-10-21 23:45:39 +0100

    Added project image to projecr view page

--- web/modules/project_view.php
+++ web/modules/project_view.php
@@ -1,6 +1,11 @@
 <div class="container pt-6">
   <div class="row pb-6">
     <div class="col-md-12 bg-white pb-4">
+      <div class="text-center pt-4 mw-35 mh-20">
+        <img class="img-fluid" style="max-height: 300px;"
+          src="/web/assets/images/<?=$project->image?>" alt="<?=$project->title?> image"
+        />
+      </div>
       <?=$project->content?>
     </div>
   </div>
