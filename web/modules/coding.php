6304a73c976dee175ff519646b2964ee0964425b
Ren Delaney <ren.delaney@gmail.com>
2020-12-10 15:17:55 +0000

    updated text on coding page and setup sub-heading explaining experience

--- web/modules/coding.php
+++ web/modules/coding.php
@@ -4,23 +4,35 @@
 
       <div class="card card-primary bg-dark">
         <div class="card-header">
-          <h3 class="card-title"><i class="zmdi zmdi-code"></i>&nbsp; PHP</h3>
+          <h1 class="card-title"><i class="zmdi zmdi-code"></i>&nbsp; PHP</h1>
+        </div>
+        <div class="card-sub-header">
+          <h4 class="card-title">2 Years Commercial Experience</h4>
         </div>
         <div class="card-body">
           <p class="text-center">
-            I have 2 years of experience using PHP with JS, Ajax, jQuery and
-            phpMyAdmin to create web applications such as shop, inventory
-            management, bookings & article systems. Worked on major projects with
-            my team by following Scrum / Agile methodologies and the use of
-            Jira Kanban boards to deliver projects in a timely and efficient
-            manner.
+            I have 2 years of experience as a main software engineer at Inkerman
+            Technologies where I worked on over 15
+            different bespoke systems for clients in various industries, such as
+            ticket booking systems, dance school systems and e-commerce systems,
+            in a fast-paced development schedule using PHP with JS, Ajax, jQuery,
+            MySQL and the Laravel framework. I deployed these systems to Linux cloud machines.
+            <br><br>
+            I worked on both the front and back-end of major projects with my
+            team by following Scrum and Agile methodologies using Jira Kanban
+            boards to deliver projects in a timely & efficient manner.  In my
+            2 years of working at Inkerman Technologies, I grew more confident
+            in my coding abilities and increased my skill set a great deal.
           </p>
         </div>
       </div>
 
       <div class="card card-primary bg-dark">
         <div class="card-header">
-          <h3 class="card-title"><i class="zmdi zmdi-language-python"></i>&nbsp; Python</h3>
+          <h1 class="card-title"><i class="zmdi zmdi-language-python"></i>&nbsp; Python</h1>
+        </div>
+        <div class="card-sub-header">
+          <h4 class="card-title">Experience creating small web-scraper projects and bots</h4>
         </div>
         <div class="card-body">
           <p class="text-center">
@@ -47,6 +59,18 @@
             <img style="max-width:100%; border: 2px solid white;" src="https://ren.ie/web/assets/images/github/rapuli-bot/rapuli-bot-example.png" />
             <br><small>Image of Rapuli-Bot in action, returning custom links to two users</small>
           </p>
+        </div>
+      </div>
+
+      <div class="card card-primary bg-dark">
+        <div class="card-header">
+          <h1 class="card-title"><i class="zmdi zmdi-code"></i>&nbsp; Java</h1>
+        </div>
+        <div class="card-sub-header">
+          <h4 class="card-title">Used throughout my 4 years at college</h4>
+        </div>
+        <div class="card-body">
+
         </div>
       </div>
 
