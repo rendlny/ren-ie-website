e4f35133f6368c6edbc91ddd0f3d3cb400b648cc
Ren Delaney <ren.delaney@gmail.com>
2021-02-02 00:12:52 +0000

    Updated gallery mobile view to show folders clearly

--- web/modules/gallery.php
+++ web/modules/gallery.php
@@ -5,7 +5,12 @@ if($galleryFolders != NULL){
   foreach ($galleryFolders as $gSection) {
     $galleryList .= '
     <div class="col-lg-4 col-md-6 masonry-item">
-      <div class="card">
+      <div class="card card-royal">
+        <a href="/gallery/'.$gSection->slug.'">
+          <div class="card-header mobile-only">
+            <h3 class="card-title"><i class="fa fa-folder"></i> '.$gSection->title.'</h3>
+          </div>
+        </a>
         <figure class="ms-thumbnail ms-thumbnail-horizontal">
           <a href="/gallery/'.$gSection->slug.'">
           <img src="/web/assets/images/'.$gSection->image.'" alt="'.$gSection->title.' cover image" class="img-fluid">
@@ -26,7 +31,6 @@ if($galleryFolders != NULL){
   <div class="row masonry-container">
     <div class="col-md-12">
       <?=$galleryList?>
-
     </div>
   </div>
 </div>
