fd84f751083bf63acf9e7dccc4f829e4e83ff667
Ren Delaney <ren.delaney@gmail.com>
2021-10-01 13:30:50 +0100

    Fixed goodreads wishlist widget in wrong order

--- web/widgets/goodreads_wishlist.php
+++ web/widgets/goodreads_wishlist.php
@@ -1,98 +1,92 @@
 <h1><i class="fas fa-heart"></i> Wish-List</h1>
 
-<div id="gr_custom_widget_1632262181">
-          <div class="gr_custom_container_1632262181">
-    <h2 class="gr_custom_header_1632262181">
-    <a style="text-decoration: none;" rel="nofollow" href="https://www.goodreads.com/review/list/96492908-ren?shelf=wish-list&amp;utm_medium=api&amp;utm_source=custom_widget">Wish-List</a>
+<div id="gr_custom_widget_1633090814">
+  <div class="gr_custom_container_1633090814">
+    <h2 class="gr_custom_header_1633090814">
+      <a style="text-decoration: none;" rel="nofollow" href="https://www.goodreads.com/review/list/96492908-ren?shelf=wish-list&amp;utm_medium=api&amp;utm_source=custom_widget">Wish-List</a>
     </h2>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="The Art of Nausicaä of the Valley of the Wind: Watercolor Impressions" rel="nofollow" href="https://www.goodreads.com/review/show/4250393423?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Art of Nausicaä of the Valley of the Wind: Watercolor Impressions" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1377214798l/766111._SX98_.jpg" /></a>
-          </div>
+    <div class="gr_custom_each_container_1633090814">
+      <div class="gr_custom_book_container_1633090814">
+        <a title="The Eye of the World (The Wheel of Time, #1)" rel="nofollow" href="https://www.goodreads.com/review/show/4187242031?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Eye of the World" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1337818095l/228665._SX98_.jpg" /></a>
       </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Nausicaä of the Valley of the Wind, Vol. 2 (Nausicaä of the Valley of the Wind, #2)" rel="nofollow" href="https://www.goodreads.com/review/show/4250393059?utm_medium=api&amp;utm_source=custom_widget"><img alt="Nausicaä of the Valley of the Wind, Vol. 2" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388260678l/732139._SX98_.jpg" /></a>
-          </div>
+    </div>
+    <div class="gr_custom_each_container_1633090814">
+      <div class="gr_custom_book_container_1633090814">
+        <a title="Dune (Dune, #1)" rel="nofollow" href="https://www.goodreads.com/review/show/4250385475?utm_medium=api&amp;utm_source=custom_widget"><img alt="Dune" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1631783289l/59007047._SY160_.jpg" /></a>
       </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Nausicaä of the Valley of the Wind: The Complete Series" rel="nofollow" href="https://www.goodreads.com/review/show/4250392976?utm_medium=api&amp;utm_source=custom_widget"><img alt="Nausicaä of the Valley of the Wind: The Complete Series" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1346688306l/15800525._SX98_.jpg" /></a>
-          </div>
+    </div>
+    <div class="gr_custom_each_container_1633090814">
+      <div class="gr_custom_book_container_1633090814">
+        <a title="The Priory of the Orange Tree" rel="nofollow" href="https://www.goodreads.com/review/show/3104471992?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Priory of the Orange Tree" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1527807139l/40275288._SX98_.jpg" /></a>
       </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Vagrant Queen Vol. 1" rel="nofollow" href="https://www.goodreads.com/review/show/3728622442?utm_medium=api&amp;utm_source=custom_widget"><img alt="Vagrant Queen Vol. 1" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1543382473l/42970481._SX98_.jpg" /></a>
-          </div>
+    </div>
+    <div class="gr_custom_each_container_1633090814">
+      <div class="gr_custom_book_container_1633090814">
+        <a title="Cosmoknights (Cosmoknights, #1)" rel="nofollow" href="https://www.goodreads.com/review/show/3223257065?utm_medium=api&amp;utm_source=custom_widget"><img alt="Cosmoknights" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546497784l/43419730._SX98_.jpg" /></a>
       </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="The Ones We're Meant to Find" rel="nofollow" href="https://www.goodreads.com/review/show/3953593468?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Ones We're Meant to Find" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1600354595l/44084665._SX98_.jpg" /></a>
-          </div>
+    </div>
+    <div class="gr_custom_each_container_1633090814">
+      <div class="gr_custom_book_container_1633090814">
+        <a title="The Art of Nausicaä of the Valley of the Wind" rel="nofollow" href="https://www.goodreads.com/review/show/3827713368?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Art of Nausicaä of the Valley of the Wind" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1545518410l/40944445._SX98_.jpg" /></a>
       </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Flung Out of Space: Inspired by the Indecent Adventures of Patricia Highsmith" rel="nofollow" href="https://www.goodreads.com/review/show/3933960651?utm_medium=api&amp;utm_source=custom_widget"><img alt="Flung Out of Space: Inspired by the Indecent Adventures of Patricia Highsmith" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1618666249l/56969413._SX98_.jpg" /></a>
-          </div>
+    </div>
+    <div class="gr_custom_each_container_1633090814">
+      <div class="gr_custom_book_container_1633090814">
+        <a title="Flung Out of Space: Inspired by the Indecent Adventures of Patricia Highsmith" rel="nofollow" href="https://www.goodreads.com/review/show/3933960651?utm_medium=api&amp;utm_source=custom_widget"><img alt="Flung Out of Space: Inspired by the Indecent Adventures of Patricia Highsmith" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1618666249l/56969413._SX98_.jpg" /></a>
       </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Steven Universe: End of an Era" rel="nofollow" href="https://www.goodreads.com/review/show/3827714444?utm_medium=api&amp;utm_source=custom_widget"><img alt="Steven Universe: End of an Era" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1581173719l/50157960._SX98_.jpg" /></a>
-          </div>
-      </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="The Art of Nausicaä of the Valley of the Wind" rel="nofollow" href="https://www.goodreads.com/review/show/3827713368?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Art of Nausicaä of the Valley of the Wind" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1545518410l/40944445._SX98_.jpg" /></a>
-          </div>
-      </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Steelheart (The Reckoners, #1)" rel="nofollow" href="https://www.goodreads.com/review/show/3561749014?utm_medium=api&amp;utm_source=custom_widget"><img alt="Steelheart" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1357576738l/17182126._SX98_.jpg" /></a>
-          </div>
-      </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Land of the Lustrous, Vol. 11" rel="nofollow" href="https://www.goodreads.com/review/show/3546210944?utm_medium=api&amp;utm_source=custom_widget"><img alt="Land of the Lustrous, Vol. 11" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1624829664l/50363591._SX98_.jpg" /></a>
-          </div>
-      </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Dark One: Vol. 1" rel="nofollow" href="https://www.goodreads.com/review/show/3483923337?utm_medium=api&amp;utm_source=custom_widget"><img alt="Dark One: Vol. 1" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1614747181l/27108939._SX98_.jpg" /></a>
-          </div>
-      </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Cosmoknights (Cosmoknights, #1)" rel="nofollow" href="https://www.goodreads.com/review/show/3223257065?utm_medium=api&amp;utm_source=custom_widget"><img alt="Cosmoknights" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546497784l/43419730._SX98_.jpg" /></a>
-          </div>
-      </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="Dune (Dune, #1)" rel="nofollow" href="https://www.goodreads.com/review/show/4250385475?utm_medium=api&amp;utm_source=custom_widget"><img alt="Dune" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1631783289l/59007047._SY160_.jpg" /></a>
-          </div>
-      </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="The Eye of the World (The Wheel of Time, #1)" rel="nofollow" href="https://www.goodreads.com/review/show/4187242031?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Eye of the World" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1337818095l/228665._SX98_.jpg" /></a>
-          </div>
-      </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
+    </div>
+    <div class="gr_custom_each_container_1633090814">
+          <div class="gr_custom_book_container_1633090814">
             <a title="The Traitor Baru Cormorant (The Masquerade, #1)" rel="nofollow" href="https://www.goodreads.com/review/show/3806005936?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Traitor Baru Cormorant" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1422463936l/23444482._SX98_.jpg" /></a>
           </div>
       </div>
-      <div class="gr_custom_each_container_1632262181">
-          <div class="gr_custom_book_container_1632262181">
-            <a title="The Priory of the Orange Tree" rel="nofollow" href="https://www.goodreads.com/review/show/3104471992?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Priory of the Orange Tree" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1527807139l/40275288._SX98_.jpg" /></a>
+      <div class="gr_custom_each_container_1633090814">
+          <div class="gr_custom_book_container_1633090814">
+            <a title="Nausicaä of the Valley of the Wind, Vol. 2 (Nausicaä of the Valley of the Wind, #2)" rel="nofollow" href="https://www.goodreads.com/review/show/4250393059?utm_medium=api&amp;utm_source=custom_widget"><img alt="Nausicaä of the Valley of the Wind, Vol. 2" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388260678l/732139._SX98_.jpg" /></a>
           </div>
       </div>
-  <br style="clear: both"/>
-  <center>
-    <a rel="nofollow" href="https://www.goodreads.com/"><img alt="goodreads.com" style="border:0" src="https://s.gr-assets.com/images/widget/widget_logo.gif" /></a>
-  </center>
-  <noscript>
-    Share <a rel="nofollow" href="https://www.goodreads.com/">book reviews</a> and ratings with Ren, and even join a <a rel="nofollow" href="https://www.goodreads.com/group">book club</a> on Goodreads.
-  </noscript>
-  </div>
-
+      <div class="gr_custom_each_container_1633090814">
+          <div class="gr_custom_book_container_1633090814">
+            <a title="Dark One: Vol. 1" rel="nofollow" href="https://www.goodreads.com/review/show/3483923337?utm_medium=api&amp;utm_source=custom_widget"><img alt="Dark One: Vol. 1" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1614747181l/27108939._SX98_.jpg" /></a>
+          </div>
       </div>
-      <script src="https://www.goodreads.com/review/custom_widget/96492908.Wish-List?cover_position=middle&cover_size=medium&num_books=30&order=d&shelf=wish-list&show_author=0&show_cover=1&show_rating=0&show_review=0&show_tags=0&show_title=0&sort=position&widget_bg_color=FFFFFF&widget_bg_transparent=true&widget_border_width=1&widget_id=1632262181&widget_text_color=000000&widget_title_size=medium&widget_width=medium" type="text/javascript" charset="utf-8"></script>
+      <div class="gr_custom_each_container_1633090814">
+          <div class="gr_custom_book_container_1633090814">
+            <a title="Land of the Lustrous, Vol. 11" rel="nofollow" href="https://www.goodreads.com/review/show/3546210944?utm_medium=api&amp;utm_source=custom_widget"><img alt="Land of the Lustrous, Vol. 11" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1624829664l/50363591._SX98_.jpg" /></a>
+          </div>
+      </div>
+      <div class="gr_custom_each_container_1633090814">
+          <div class="gr_custom_book_container_1633090814">
+            <a title="Steven Universe: End of an Era" rel="nofollow" href="https://www.goodreads.com/review/show/3827714444?utm_medium=api&amp;utm_source=custom_widget"><img alt="Steven Universe: End of an Era" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1581173719l/50157960._SX98_.jpg" /></a>
+          </div>
+      </div>
+      <div class="gr_custom_each_container_1633090814">
+          <div class="gr_custom_book_container_1633090814">
+            <a title="Vagrant Queen Vol. 1" rel="nofollow" href="https://www.goodreads.com/review/show/3728622442?utm_medium=api&amp;utm_source=custom_widget"><img alt="Vagrant Queen Vol. 1" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1543382473l/42970481._SX98_.jpg" /></a>
+          </div>
+      </div>
+      <div class="gr_custom_each_container_1633090814">
+          <div class="gr_custom_book_container_1633090814">
+            <a title="The Art of Nausicaä of the Valley of the Wind: Watercolor Impressions" rel="nofollow" href="https://www.goodreads.com/review/show/4250393423?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Art of Nausicaä of the Valley of the Wind: Watercolor Impressions" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1377214798l/766111._SX98_.jpg" /></a>
+          </div>
+      </div>
+    <div class="gr_custom_each_container_1633090814">
+      <div class="gr_custom_book_container_1633090814">
+        <a title="The Ones We're Meant to Find" rel="nofollow" href="https://www.goodreads.com/review/show/3953593468?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Ones We're Meant to Find" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1600354595l/44084665._SX98_.jpg" /></a>
+      </div>
+    </div>
+    <div class="gr_custom_each_container_1633090814">
+      <div class="gr_custom_book_container_1633090814">
+        <a title="Nausicaä of the Valley of the Wind: The Complete Series" rel="nofollow" href="https://www.goodreads.com/review/show/4250392976?utm_medium=api&amp;utm_source=custom_widget"><img alt="Nausicaä of the Valley of the Wind: The Complete Series" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1346688306l/15800525._SX98_.jpg" /></a>
+      </div>
+    </div>
+    <br style="clear: both"/>
+    <center>
+      <a rel="nofollow" href="https://www.goodreads.com/"><img alt="goodreads.com" style="border:0" src="https://s.gr-assets.com/images/widget/widget_logo.gif" /></a>
+    </center>
+    <noscript>
+      Share <a rel="nofollow" href="https://www.goodreads.com/">book reviews</a> and ratings with Ren, and even join a <a rel="nofollow" href="https://www.goodreads.com/group">book club</a> on Goodreads.
+    </noscript>
+  </div>
+</div>
+<script src="https://www.goodreads.com/review/custom_widget/96492908.Wish-List?cover_position=middle&cover_size=medium&num_books=30&order=a&shelf=wish-list&show_author=0&show_cover=1&show_rating=0&show_review=0&show_tags=0&show_title=0&sort=position&widget_bg_color=FFFFFF&widget_bg_transparent=true&widget_border_width=1&widget_id=1633090814&widget_text_color=000000&widget_title_size=medium&widget_width=medium" type="text/javascript" charset="utf-8"></script>
