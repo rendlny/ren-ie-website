6d88e5e6c89a1757bbc998e35b5ba58fd757b5d5
Ren Delaney <ren.delaney@gmail.com>
2021-10-12 23:44:46 +0100

    fixed soundcloud link to my tracks

--- web/modules/music.php
+++ web/modules/music.php
@@ -7,16 +7,16 @@
       <div class="card card-primary bg-dark mb-4">
         <div class="card-body">
           <p class="text-center">
-            In my freetime I like to mess around with my synth setup ( a Keystep
+            In my free-time I like to mess around with my synth setup ( a Keystep
             hooked up to a Volca Keys with some effects pedals ) and try to make some
-            music, below is one of the tracks that I've created. I am inpired by music from
+            music, below is one of the tracks that I've created. I am inspired by music from
             Para One's Naissance Des Pieuvres album,  scntfc's work in Oxenfree & Hainbach.
             I'm a big fan of synthy & ambient movie scores and video game soundtracks.<br>
             Check out some of the tracks I've created below!
           </p>
-          <a href="https://soundcloud.com/rendly" target ="blank" class="btn btn-lg btn-block btn-primary btn-raised" style="background: #ff5500;">
+          <a href="https://soundcloud.com/rendly/popular-tracks" target ="blank" class="btn btn-lg btn-block btn-primary btn-raised" style="background: #ff5500;">
             <i class="fa fa-external-link"></i>
-            My Soundcloud
+            My SoundCloud
         </a>
         </div>
       </div>
