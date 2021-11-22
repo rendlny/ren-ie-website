bf9d747c36206644051bd092aedc025ebfe70f7d
Ren Delaney <ren.delaney@gmail.com>
2021-11-22 00:52:40 +0000

    added army unit details and basic img display

--- web/modules/warhammer.php
+++ web/modules/warhammer.php
@@ -1,27 +1,20 @@
+<?php
+$armyJsonFile = file_get_contents("../assets/json/army-lists.json");
+$armyJsonData = json_decode($armyJsonFile);
+?>
 <div class="container pt-4">
   <div class="row">
 
     <div class="col-md-12 col-lg-12">
 
       <div class="card card-primary bg-dark">
-        <div class="card-header">
-          <h3 class="card-title"><i class="fa fa-skull"></i>&nbsp; Warhammer 40,000</h3>
-        </div>
         <div class="card-body">
 
-            <div class="army-banner-border"></div>
-            <div class="army-banner">
-                <div class="army-title">Death Korps of Krieg</div>
-            </div>
-            <div class="army-banner-border"></div>
-
-            <br/>
-            <br/>
-            <div class="army-banner-border"></div>
-            <div class="army-banner">
-                <div class="army-title">Space Wolves</div>
-            </div>
-            <div class="army-banner-border"></div>
+          <?php 
+            foreach($armyJsonData as $armyName => $army) {
+              include '../includes/warhammer_army_card.php';
+            }
+          ?>
 
         </div>
       </div>
