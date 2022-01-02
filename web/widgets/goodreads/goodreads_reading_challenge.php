f34991d846e2e140d821bf4b3a662f8888f0034a
Ren Delaney <ren.delaney@gmail.com>
2022-01-02 04:49:14 +0000

    added new 2022 reading challenge widget

--- web/widgets/goodreads/goodreads_reading_challenge.php
+++ web/widgets/goodreads/goodreads_reading_challenge.php
@@ -1,41 +1,33 @@
-<h1><i class="fa fa-trophy"></i> 2021 Reading Challenge</h1>
+
+<h1><i class="fa fa-trophy"></i> 2022 Reading Challenge</h1>
 
 <div style="text-align:center;">
   <div id="gr_challenge_container">
-    <div id="gr_challenge_11650" style="border: 2px solid #EBE8D5; border-radius:10px; padding: 0px 7px 0px 7px; max-width:230px; min-height: 100px">
-      <div id="gr_challenge_progress_body_11650" style="font-size: 12px; font-family: georgia,serif;line-height: 18px">
+    <div id="gr_challenge_11636" style="border: 2px solid #EBE8D5; border-radius:10px; padding: 0px 7px 0px 7px; max-width:230px; min-height: 100px">
+      <div id="gr_challenge_progress_body_11636" style="font-size: 12px; font-family: georgia,serif;line-height: 18px">
         <h3 style="margin: 4px 0 10px; font-weight: normal; text-align: center">
-          <a style="text-decoration: none; font-family:georgia,serif;font-style:italic; font-size: 1.1em" rel="nofollow" href="https://www.goodreads.com/challenges/11650-2021-reading-challenge">
-            2021 Reading Challenge
-          </a>
+          <a style="text-decoration: none; font-family:georgia,serif;font-style:italic; font-size: 1.1em" rel="nofollow" href="https://www.goodreads.com/challenges/11636-2022-reading-challenge">2022 Reading Challenge</a>
         </h3>
-        <div class="challengePic">
-          <a rel="nofollow" href="https://www.goodreads.com/challenges/11650-2021-reading-challenge">
-            <img alt="2021 Reading Challenge" style="float:left; margin-right: 10px; border: 0 none" src="https://images.gr-assets.com/challenges/1608245803p2/11650.jpg" />
-          </a>
-        </div>
-        <div>
-          <a rel="nofollow" href="https://www.goodreads.com/user/show/96492908-ren">Ren</a> 
-          has read 0 books toward
-          their goal of 20 books.
-        </div>
-        <div style="width: 100px; margin: 4px 5px 5px 0; float: left; border: 1px solid #382110; height: 8px; overflow: hidden; background-color: #FFF">
-          <div style="width: 0%; background-color: #D7D2C4; float: left">
-            <span style="visibility:hidden">hide</span>
+            <div class="challengePic">
+              <a rel="nofollow" href="https://www.goodreads.com/challenges/11636-2022-reading-challenge"><img alt="2022 Reading Challenge" style="float:left; margin-right: 10px; border: 0 none" src="https://images.gr-assets.com/challenges/1636444409p2/11636.jpg" /></a>
+            </div>
+          <div>
+            <a rel="nofollow" href="https://www.goodreads.com/user/show/96492908-ren">Ren</a> has
+                read 0 books toward
+                their goal of
+                30 books.
           </div>
-        </div>
-        <div style="font-family: arial, verdana, helvetica, sans-serif;font-size:90%">
-          <a rel="nofollow" href="https://www.goodreads.com/user_challenges/25456891">
-            0 of 20 (0%)
-          </a>
-        </div>
-        <div style="text-align: right;">
-          <a style="text-decoration: none; font-size: 10px;" rel="nofollow" href="https://www.goodreads.com/user_challenges/25456891">
-            view books
-          </a>
-        </div>
+          <div style="width: 100px; margin: 4px 5px 5px 0; float: left; border: 1px solid #382110; height: 8px; overflow: hidden; background-color: #FFF">
+            <div style="width: 0%; background-color: #D7D2C4; float: left"><span style="visibility:hidden">hide</span></div>
+          </div>
+          <div style="font-family: arial, verdana, helvetica, sans-serif;font-size:90%">
+            <a rel="nofollow" href="https://www.goodreads.com/user_challenges/31971822">0 of 30 (0%)</a>
+          </div>
+            <div style="text-align: right;">
+              <a style="text-decoration: none; font-size: 10px;" rel="nofollow" href="https://www.goodreads.com/user_challenges/31971822">view books</a>
+            </div>
       </div>
-      <script src="https://www.goodreads.com/user_challenges/widget/96492908-ren?challenge_id=11650&v=2"></script>
+      <script src="https://www.goodreads.com/user_challenges/widget/96492908-ren?challenge_id=11636&v=2"></script>
     </div>
   </div>
 </div>
