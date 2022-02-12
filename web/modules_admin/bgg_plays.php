eb7280f77cb4fee4a1201e66f0c507054d40c8c1
Ren Delaney <ren.delaney@gmail.com>
2022-02-12 19:36:53 +0000

    figured out how to read xml data

--- web/modules_admin/bgg_plays.php
+++ web/modules_admin/bgg_plays.php
@@ -2,22 +2,51 @@
 $file = '../assets/xml/bgg_plays.xml';
 if (file_exists($file)) {
     $plays = simplexml_load_file($file);
-    foreach($plays as $play) {
-        $players = $play->players;
-        foreach($players as $player) {
-            echo $player->username.'<br>';
-            // echo $player->username.'<br>';
-            // echo $key.'<br>';
-            // echo $player->getUsername();
-            // $username = $player->username[0]->__toString();
-            // print_r($player->username);
-            // echo($username);
-            // print_r($player);
+    echo($plays['username'].'<br>');
+    echo($plays['userid'].'<br>');
+    echo($plays['total'].'<br>');
+    echo($plays['page'].'<br>');
+    echo('<br>---<br>');
+    foreach($plays->children() as $play) {
+        
+        echo '<br>=======play========================<br>';
+        echo $play['id'].'<br>';
+        echo $play['date'].'<br>';
+        echo $play['quantity'].'<br>';
+        echo $play['length'].'<br>';
+        echo $play['incomplete'].'<br>';
+        echo $play['nowinstats'].'<br>';
+        echo $play['location'].'<br>';
 
+        foreach($play->children() as $playData) {
+
+            if($playData->getName() == 'item') {
+                echo($playData['name'].'<br>');
+                echo($playData['objecttype'].'<br>');
+                echo($playData['objectid'].'<br>');
+                foreach($playData->children() as $subtypes) {
+                    foreach($subtypes->children() as $subtype) {
+                        echo($subtype['value'].'<br>');
+                    }
+                }
+            } else if ($playData->getName() == 'comments') {
+                echo($playData->__toString());
+            } else if ($playData->getName() == 'players') {
+                echo('players = ');
+                foreach($playData->children() as $player) {
+                    echo($player['name'].'<br>');
+                    echo($player['username'].'<br>');
+                    echo($player['userid'].'<br>');
+                    echo($player['startposition'].'<br>');
+                    echo($player['color'].'<br>');
+                    echo($player['score'].'<br>');
+                    echo($player['new'].'<br>');
+                    echo($player['rating'].'<br>');
+                    echo($player['win'].'<br>');
+                }
+            }
+            echo('<br>');
         }
-        // echo($play->id);
-        // print_r($play->date);
-        echo '----';
     }
 } 
 else {
