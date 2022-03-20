0952527732d4fc95c7c835a713f6bf348163a1a6
Ren Delaney <ren.delaney@gmail.com>
2022-03-20 14:03:38 +0000

    fix include location for server

--- web/widgets/bgg/bgg_leader_board.php
+++ web/widgets/bgg/bgg_leader_board.php
@@ -11,7 +11,7 @@ $players = Player::where('on_leader_board', true)->orderBy('wins', 'DESC')->get(
         <div class="col-md-12">
             <?
                 foreach($players as $player) {
-                    include '../widgets/bgg/bgg_player_score_row.php';
+                    include $_SERVER['DOCUMENT_ROOT'].'/web/widgets/bgg/bgg_player_score_row.php';
                 }
             ?>
         </div>
