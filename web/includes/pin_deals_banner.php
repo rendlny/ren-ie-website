8ae7ee6621c7da78a680a5a3f0aa4cd1aae31516
Ren Delaney <ren.delaney@gmail.com>
2022-08-28 22:05:23 +0100

    updated pricing and pin deal info

--- web/includes/pin_deals_banner.php
+++ web/includes/pin_deals_banner.php
@@ -1,8 +1,8 @@
 <a href="#pin-deals">
     <div class="text-center p-4" style="background-color: #9c27b0; display:block;">
-        <h1 style="margin:0px;">
-            New Pin Deals! savings when buying multiple pins<br>
+        <h3 style="margin:0px;">
+            Pin Deals! savings when buying multiple pins<br>
             <small>Click for more info</small>
-        </h1>
+        </h3>
     </div>
 </a>
